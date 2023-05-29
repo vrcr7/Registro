aaba4f2d2101b63d67af087d00a93cb9ba82574b
vrcr7 <fcontreras.dev@gmail.com>
2023-05-28 23:11:26 -0400

    completo con validaciones. actividad 6

--- app/src/main/java/com/example/registro/MainActivity.java
+++ app/src/main/java/com/example/registro/MainActivity.java
@@ -12,6 +12,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
     Button enviar;
@@ -40,26 +43,33 @@ public class MainActivity extends AppCompatActivity {
                 Context context =getApplicationContext();
                 CharSequence text="Datos vacios";
                 CharSequence text2="error de password";
+                CharSequence text3="solo se permiten letras, nombre y apellido no validos";
                 int duracion = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context,text,duracion);
                 Toast toast2 = Toast.makeText(context,text2,duracion);
+                Toast toast3 = Toast.makeText(context,text3,duracion);
 
                 if ((!nombre.getText().toString().trim().equals("")) && (!apellido.getText().toString().trim().equals("")) && (!correo.getText().toString().trim().equals(""))){
-                 if(password.getText().toString().equals(pass)){
-                     //redireccionar a la activity 2
-                     Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                     intent.putExtra("name", nombre.getText().toString());
-                     intent.putExtra("surname", apellido.getText().toString());
-                     intent.putExtra("email", correo.getText().toString());
-                     startActivity ( intent );
-                 }
-                     else{
-                         toast2.show();
-                     }
-                    }
-                else {
-                    toast.show();
+                    Pattern pat = Pattern.compile("[a-zA-Z]{2,10}");
+               //     Pattern pat2 = Pattern.compile("[0-9]*{2,10}");
+                    Matcher mat = pat.matcher(nombre.getText().toString());
+                    Matcher mat2 = pat.matcher(apellido.getText().toString());
+                  //  Matcher mat3 = pat.matcher(variable.getText().toString());
+
+                    if(mat.matches() && mat2.matches())
+                        if (password.getText().toString().equals(pass)) {
+                            //redireccionar a la activity 2
+                            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                            intent.putExtra("name", nombre.getText().toString());
+                            intent.putExtra("surname", apellido.getText().toString());
+                            intent.putExtra("email", correo.getText().toString());
+                            startActivity(intent);
+                        } else {
+                            toast2.show();
+                        }
+                    else { toast3.show();}
                 }
+                else {toast.show();}
             }
         });
 
